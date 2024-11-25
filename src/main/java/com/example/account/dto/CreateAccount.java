80bbac955cf99dcb19fff38a40947f60a7989c72
zeomzzz <jmlee2705@gmail.com>
2024-11-25 22:20:09 +0900

    feat: CreateAccount dto 생성

---
+++ src/main/java/com/example/account/dto/CreateAccount.java
@@ -0,0 +1,28 @@
+package com.example.account.dto;
+
+import java.time.LocalDateTime;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+public class CreateAccount {
+    @Getter
+    @Setter
+    public static class Request {
+        private Long userId;
+        private Long intialBalance;
+    }
+
+    @Getter
+    @Setter
+    @NoArgsConstructor
+    @AllArgsConstructor
+    @Builder
+    public static class Response {
+        private Long userId;
+        private String accountNumber;
+        private LocalDateTime registedAt;
+    }
+}
