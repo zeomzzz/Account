dde3426cf1443288ed850cfb01cedbbbe12ce650
zeomzzz <jmlee2705@gmail.com>
2024-11-26 01:18:15 +0900

    feat: 사용자 정보 생성 sql 작성

---
+++ src/main/resources/data.sql
@@ -0,0 +1,10 @@
+insert into account_user(id, name, created_at, updated_at)
+values (1, "Aaaa"), now(), now());
+insert into account_user(id, name, created_at, updated_at)
+values (1, "Baaa"), now(), now());
+insert into account_user(id, name, created_at, updated_at)
+values (1, "Caaa"), now(), now());
+insert into account_user(id, name, created_at, updated_at)
+values (1, "Daaa"), now(), now());
+insert into account_user(id, name, created_at, updated_at)
+values (1, "Eaaa"), now(), now());
